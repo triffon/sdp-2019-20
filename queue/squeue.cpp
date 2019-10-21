6a76395abdbdb59ec7b34d51de716582d4346cd8
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2019-10-21 10:23:33 +0300

    Завършване на последователната реализация на опашка

--- queue/squeue.cpp
+++ queue/squeue.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "abstract_queue.h"
 
 const unsigned MAX_QUEUE = 100;
@@ -7,23 +8,66 @@ class StaticQueue : public AbstractQueue<T> {
 
   T a[MAX_QUEUE];
   int front, back;
+
+  // проверка дали масивът е запълнен
+  bool full() const { return back == MAX_QUEUE - 1; }
   
 public:
-  StaticQueue() : back(-1), front(0) {}
+  StaticQueue() : front(0), back(-1) {}
   
   // проверка за празнота на опашка
   bool empty() const { return front == back + 1; }
 
   // включване на елемент и връща дали е било успешно
-  bool enqueue(T const& x) { return true; }
+  bool enqueue(T const& x);
 
   // изключване на елемент
-  T dequeue() { return T(); }
+  T dequeue();
 
   // поглеждане в началото на опашката
-  T const& head() const { return T(); }
+  T const& head() const;
 
   // поглеждане в началото на опашката с възможност за промяна
-  T& head() { return a[0]; } // !!! лошо !!!
+  T& head();
   
 };
+
+template <typename T>
+bool StaticQueue<T>::enqueue(T const& x) {
+  if (full()) {
+    std::cerr << "Опит за добавяне в пълна опашка!\n";
+    return false;
+  }
+  
+  a[++back] = x;
+  return true;
+}
+
+template <typename T>
+T StaticQueue<T>::dequeue() {
+  if (empty()) {
+    std::cerr << "Опит за изваждане от празна опашка!\n";
+    return T();
+  }
+  return a[front++];
+}
+
+template <typename T>
+T const& StaticQueue<T>::head() const {
+  if (empty()) {
+    std::cerr << "Опит за поглеждане в празна опашка!\n";
+    return a[0]; // лошо!!!
+  }
+  
+  return a[front];
+}
+
+template <typename T>
+T& StaticQueue<T>::head() {
+  if (empty()) {
+    std::cerr << "Опит за поглеждане в празна опашка!\n";
+    return a[0]; // лошо!!!
+  }
+  
+  return a[front];
+}
