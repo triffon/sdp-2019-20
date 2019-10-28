9596253eed81921347354e32722b8cf71cf545d1
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2019-10-28 12:00:09 +0200

    Първоначална реализация на едносвързан списък

---
+++ lists/lists.cpp
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "llist.cpp"
+
+using TestList = LinkedList<int>;
+
+#include "list_tests.h"
--- lists/llist.cpp
+++ lists/llist.cpp
@@ -1,26 +1,112 @@
-template <typename T, typename P>
+template <typename T>
+struct LinkedListElement {
+  T data;
+  LinkedListElement* next;
+};
+
+template <typename T>
+class LinkedList;
+
+template <typename T>
+class LinkedListIterator {
+  friend class LinkedList<T>;
+  using LLE = LinkedListElement<T>;
+  using I   = LinkedListIterator<T>;
+  LLE* ptr;
+public:
+  LinkedListIterator(LLE* p = nullptr) : ptr(p) {}
+
+  I next() const {
+    if (!valid())
+      return *this;
+    
+    return I(ptr->next);
+  }
+
+  bool valid() const { return ptr != nullptr; }
+
+  T const& getConst() const { return ptr->data; } // !!! не правим проверка за коректност
+  T&       get()      const { return ptr->data; } // !!! не правим проверка за коректност
+
+  // it <-> it.valid();
+  operator bool() const { return valid(); }
+
+  // ++it
+  I& operator++() {
+    return (*this = next());
+  }
+  
+  // it++ 
+  I operator++(int) {
+    I save = *this;
+    ++(*this);
+    return save;
+  }
+
+  // *it = 3;
+  T& operator*() { return get(); }
+};
+
+template <typename T>
 class LinkedList {
+  using LLE = LinkedListElement<T>;
+  LLE *front, *back;
 public:
 
-  LinkedList();
+  using I   = LinkedListIterator<T>;
+
+  LinkedList() : front(nullptr), back(nullptr) {}
 
   LinkedList(LinkedList const& ll);
   LinkedList& operator=(LinkedList const& ll);
-  ~LinkedList();
+  ~LinkedList() {}
 
-  bool empty() const;
+  bool empty() const { return front == nullptr; }
 
-  bool insertBefore(T const& x, P const& p);
-  bool insertAfter (T const& x, P const& p);
+  bool insertBefore(I const& it, T const& x);
+  bool insertAfter (I const& it, T const& x);
 
-  bool deleteBefore(P const& p);
-  bool deleteAt    (P const& p);
-  bool deleteAfter (P const& p);
+  bool deleteBefore(I const& it);
+  bool deleteAt    (I const& it);
+  bool deleteAfter (I const& it);
 
-  bool deleteBefore(P const& p, T& x);
-  bool deleteAt    (P const& p, T& x);
-  bool deleteAfter (P const& p, T& x);
+  bool deleteBefore(I const& it, T& x);
+  bool deleteAt    (I const& it, T& x);
+  bool deleteAfter (I const& it, T& x);
 
-  T const& getAt(P const& p) const;
-  T&       getAt(P const& p);
+  bool insertFirst(T const& x) { return insertBefore(begin(), x); }
+  bool insertLast (T const& x) { return insertAfter (end()  , x); }
+
+  bool deleteFirst(T& x) { return deleteAt(begin(), x); }
+  bool deleteLast (T& x) { return deleteAt(end(),   x); }
+
+  bool deleteFirst() { return deleteAt(begin()); }
+  bool deleteLast () { return deleteAt(end());   }
+  
+  T const& getAt(I const& it) const { return it.getConst(); }
+  T&       getAt(I const& it)       { return it.get();      }
+
+  I begin() const { return I(front); }
+  I end()   const { return I(back);  }
 };
+
+template <typename T>
+bool LinkedList<T>::insertAfter(I const& it, T const& x) {
+  if (!it && !empty())
+    return false;
+
+  if (!it) {
+    // empty() == true
+    // front == back == nullptr
+    front = back = new LLE{x, nullptr};
+    return true;
+  }  
+  // it.valid()
+  
+  LLE* p = new LLE{x, it.ptr->next};
+  if (back == it.ptr)
+    // добавяме в края на списъка, трябва да преместим back
+    back = p;
+  it.ptr->next = p;
+  return true;
+}
