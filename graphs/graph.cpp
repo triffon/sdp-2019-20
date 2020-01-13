896e42d5d638c3ffe6021498521c3ca45a4b1263
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-01-13 10:02:46 +0200

    Реализация на графа

--- graphs/graph.cpp
+++ graphs/graph.cpp
@@ -1,4 +1,78 @@
 #ifndef __GRAPH_CPP
 #define __GRAPH_CPP
 
+#include <vector>
+#include "linked_hash.cpp"
+
+unsigned simpleHashFunction(int const& x) {
+  return x;
+}
+
+template <typename V>
+class Graph {
+  using VertexList = LinkedList<V>;
+  using SuccList = LinkedHash<V, VertexList, simpleHashFunction>;
+  
+  SuccList graph;
+  VertexList emptyVertexList;
+
+public:
+  std::vector<V> vertices() {
+    return graph.keys();
+  }
+
+  VertexList const& successors(V const& v) const {
+    VertexList const* succs = graph.lookupConst(v);
+    if (succs == nullptr)
+      return emptyVertexList;
+
+    return *succs;
+  }
+
+  bool addVertex(V const& v) {
+    return graph.add(v, emptyVertexList);
+  }
+
+  bool removeVertex(V const& v) {
+    // първо махаме ребрата към v
+    for(int u : vertices())
+      removeEdge(u, v);
+
+    return graph.remove(v);
+  }
+
+  bool isEdge(V const& u, V const& v) {
+    VertexList* succs = graph.lookup(u);
+    if (succs == nullptr)
+      return false;
+    for(int w : *succs)
+      if (w == v)
+        return true;
+    return false;
+  }
+
+  bool addEdge(V const& u, V const& v) {
+    VertexList* succs = graph.lookup(u);
+    if (succs == nullptr)
+      return false;
+    // искаме да го няма вече в наследниците
+    if (isEdge(u, v))
+      return false;
+    succs->insertLast(v);
+    return true;
+  }
+
+  bool removeEdge(V const& u, V const& v) {
+    VertexList* succs = graph.lookup(u);
+    if (succs == nullptr)
+      return false;
+    for(typename VertexList::I it = succs->begin(); it; ++it)
+      if (*it == v) {
+        succs->deleteAt(it);
+        return true;
+      }
+    return false;
+  }
+};
+
 #endif
