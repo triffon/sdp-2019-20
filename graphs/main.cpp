f2993751b29bb9e85749cd71c0f78c04a285a252
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-01-13 11:25:58 +0200

    Първоначална реализация на търсене в дълбочина

--- graphs/main.cpp
+++ graphs/main.cpp
@@ -1,5 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
+#include <algorithm>
+#include <iostream>
+
 #include "doctest.h"
 
 #include "graph.cpp"
@@ -33,6 +36,46 @@ bool isSymmetric(Graph<V, hashFunction>& g) {
       if (!g.isEdge(v, u))
         return false;
   return true;
+}
+
+template <typename V, HashFunction<V> hashFunction>
+bool dfsPathRec(Graph<V, hashFunction>& g, V const& u, V const& v, Path<V>& path) {
+  // дали вече този връх го има в пътя?
+  if (std::find(path.begin(), path.end(), u) != path.end())
+    return false;
+
+  // няма да зациклим, добавяме го в пътя
+  path.push_back(u);
+
+  if (u == v)
+    // намерихме път!
+    return true;
+
+  // търсим път от някой от наследниците w на u до v
+  for(V w : g.successors(u))
+    if (dfsPathRec(g, w, v, path))
+      return true;
+
+  // от никой от наследниците на u не можем да стигнем до v
+  // отказваме се от u в пътя
+
+  path.pop_back();
+  return false;
+}
+
+template <typename V>
+void printPath(Path<V> const& path) {
+  for(V v : path)
+    std::cout << v << " ";
+  std::cout << std::endl;
+}
+
+template <typename V, HashFunction<V> hashFunction>
+Path<V> dfsPath(Graph<V, hashFunction>& g, V const& u, V const& v) {
+  Path<V> result;
+  dfsPathRec(g, u, v, result);
+  printPath(result);
+  return result;
 }
 
 #include "graph_problems_tests.h"
